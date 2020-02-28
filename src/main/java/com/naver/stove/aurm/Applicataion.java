a2217afe456cd0e4091937bd259f2e68da049019
wooou <dksdnwls@gmail.com>
2020-02-28 18:35:10 +0900

    Initial commit

---
+++ src/main/java/com/naver/stove/aurm/Applicataion.java
@@ -0,0 +1,11 @@
+package com.naver.stove.aurm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applicataion {
+    public static void main(String[] args) {
+        SpringApplication.run(Applicataion.class, args);
+    }
+}
